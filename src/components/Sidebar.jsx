e6ab978e63e74c9a502fd71b385a754b49f730b7
Piyush Bansal <piyushbansal.dm007@gmail.com>
2024-08-21 11:58:03 +0530

    making sidebar component

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import Link from 'next/link';
+import React from 'react';
+import { FaTwitter } from "react-icons/fa";
+import { TbHomeFilled } from "react-icons/tb";
+
 
 export default function Sidebar() {
   return (
-    <div>
-      Sidebar
+    <div className='flex flex-col gap-4 p-3'>
+        <Link href='/'>
+            <FaTwitter className='w-16 h-16 cursor-pointer p-3 hover:bg-gray-100 rounded-full transition-all duration-200'/>
+        </Link>
+        <Link href='/' className='flex items-center p-3 hover:bg-gray-100 transition-all duration-200 gap-2 w-fit rounded-full '>
+            <TbHomeFilled className='w-7 h-7'/>
+            <span className='font-bold hidden xl:inline'>Home</span>
+        </Link>
+        <button className='bg-blue-600 text-white rounded-full hover:brightness-95 transition-all duration-200 w-48 h-9 shadow-md hidden xl:inline'>Sign In</button>
     </div>
   )
 }
