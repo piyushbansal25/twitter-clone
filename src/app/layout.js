4e113ad79930f1d33c837caedf338b2dc5d01df9
Piyush Bansal <piyushbansal.dm007@gmail.com>
2024-08-21 13:02:27 +0530

    making whats happening in news component

--- src/app/layout.js
+++ src/app/layout.js
@@ -17,9 +17,13 @@ export default function RootLayout({ children }) {
         <div className="flex justify-between max-w-6xl mx-auto">
           <div className="hidden sm:inline border-r h-screen"><Sidebar></Sidebar></div>
           <div>{children}</div>
-          <div><News/></div>
+          <div className="lg:flex-col p-3 h-screen border-l hidden lg:flex w-[24rem]">
+            <div className="sticky top-0 bg-white py-2 ">
+              <input type='text' placeholder='Search' className="bg-gray-100 border border-gray-200 rounded-3xl text-sm w-full px-4 py-2"/>
+            </div>
+            <News/></div>
         </div>
     </body>
     </html>
-  );
+  );s
 }
